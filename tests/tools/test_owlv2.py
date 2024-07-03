4f6335e7fab09a4de6d001ae1ca322b9b9a3ddfb
hrnn <hernan.payrumani@landing.ai>
2024-07-03 13:32:19 -0400

    added owlv2 tool

---
+++ tests/tools/test_owlv2.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+from vision_agent_tools.tools.owlv2 import Owlv2
+
+
+def test_successful_owlv2_detection():
+    test_image = "000000039769.jpg"
+    prompts = ["a photo of a cat", "a photo of a dog"]
+
+    image = Image.open(f"tests/tools/data/owlv2/{test_image}")
+
+    owlv2 = Owlv2()
+
+    prompts = ["a photo of a cat", "a photo of a dog"]
+
+    results = owlv2(image, prompts=prompts)
+
+    assert len(results) > 0
+
+    for pred in results:
+        assert pred.label == "a photo of a cat"
--- vision_agent_tools/tools/owlv2.py
+++ vision_agent_tools/tools/owlv2.py
@@ -1,9 +1,65 @@
+from typing import Optional
+
+import torch
+from PIL import Image
+from pydantic import BaseModel
+from transformers import Owlv2ForObjectDetection, Owlv2Processor
+
 from vision_agent_tools.tools.shared_types import BaseTool
 
+MODEL_NAME = "google/owlv2-base-patch16-ensemble"
+PROCESSOR_NAME = "google/owlv2-base-patch16-ensemble"
+DEFAULT_CONFIDENCE = 0.2
 
-class QRReader(BaseTool):
+
+class Owlv2InferenceData(BaseModel):
+    label: str
+    score: float
+    bbox: list[float]
+
+
+class Owlv2(BaseTool):
     def __init__(self):
-        pass
+        self._processor = Owlv2Processor.from_pretrained(PROCESSOR_NAME)
+        self._model = Owlv2ForObjectDetection.from_pretrained(MODEL_NAME)
 
-    def __call__(self):
-        pass
+    def __call__(
+        self,
+        image: Image.Image,
+        prompts: list[str],
+        confidence: Optional[float] = DEFAULT_CONFIDENCE,
+    ):
+        texts = [prompts]
+        # Run model inference here
+        inputs = self._processor(text=texts, images=image, return_tensors="pt")
+        # Forward pass
+        with torch.no_grad():
+            outputs = self._model(**inputs)
+
+        target_sizes = torch.Tensor([image.size[::-1]])
+
+        # Convert outputs (bounding boxes and class logits) to the final predictions type
+        results = self._processor.post_process_object_detection(
+            outputs=outputs, threshold=0.1, target_sizes=target_sizes
+        )
+        i = 0  # given that we are predicting on only one image
+        boxes, scores, labels = (
+            results[i]["boxes"],
+            results[i]["scores"],
+            results[i]["labels"],
+        )
+
+        inferences: list[Owlv2InferenceData] = []
+        for box, score, label in zip(boxes, scores, labels):
+            box = [round(i, 2) for i in box.tolist()]
+            if score < confidence:
+                continue
+            inferences.append(
+                Owlv2InferenceData(
+                    label=texts[i][label.item()], score=score.item(), bbox=box
+                )
+            )
+
+        if len(inferences) == 0:
+            return None
+        return inferences
