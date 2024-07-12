1e21406726537e6c6d371381d5d6bf1b935e3557
Camilo Iral <juan.camilo@landing.ai>
2024-07-12 14:15:02 -0500

    fix test

--- tests/tools/test_depth_anything_v2.py
+++ tests/tools/test_depth_anything_v2.py
@@ -1,12 +1,12 @@
 from PIL import Image
 
-from vision_agent_tools.tools.depth_estimation import DepthEstimation
+from vision_agent_tools.tools.depth_anything_v2 import DepthAnythingV2
 
 
 def test_successful_depth_estimation():
     test_image = "tomatoes.jpg"
     image = Image.open(f"tests/tools/data/loca/{test_image}")
 
-    depth = DepthEstimation()
+    depth = DepthAnythingV2()
     results = depth(image)
     assert len(results.map) > 0
