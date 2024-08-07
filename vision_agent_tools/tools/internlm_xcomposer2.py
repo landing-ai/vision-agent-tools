7d5ea68113d1e068a56633d1497a8f1e9e5b8c62
Camilo Iral <juan.camilo@landing.ai>
2024-08-07 12:56:45 -0500

    try

--- vision_agent_tools/tools/internlm_xcomposer2.py
+++ vision_agent_tools/tools/internlm_xcomposer2.py
@@ -2,7 +2,6 @@ import torch
 from PIL import Image
 from vision_agent_tools.types import VideoNumpy
 from vision_agent_tools.tools.shared_types import BaseTool
-import numpy as np
 
 from lmdeploy import GenerationConfig, TurbomindEngineConfig, pipeline
 from transformers.dynamic_module_utils import get_class_from_dynamic_module
@@ -10,15 +9,28 @@ from transformers.dynamic_module_utils import get_class_from_dynamic_module
 
 class InternLMXComposer2(BaseTool):
     _HF_MODEL = "internlm/internlm-xcomposer2d5-7b"
+    MAX_NUMBER_OF_FRAMES = 32
+    MAX_IMAGE_SIZE = 1024
 
-    def _transform_image(image: np.ndarray, max_size: int):
-        image = Image.fromarray(image)
+    def _transform_image(image: Image.Image, max_size: int):
         image = image.convert("RGB")
         if image.size[0] > max_size or image.size[1] > max_size:
             image.thumbnail((max_size, max_size))
         return image
 
-    def __init__(self, max_size=1024):
+    def _process_video(self, images: VideoNumpy, num_frm: int = MAX_NUMBER_OF_FRAMES):
+        if len(images) > num_frm:
+            num_frm = min(num_frm, len(images))
+            step_size = len(images) / (num_frm + 1)
+            indices = [int(i * step_size) for i in range(num_frm)]
+            images = [images[i] for i in indices]
+        images = [
+            self._transform_image(Image.fromarray(arr), self.max_size) for arr in images
+        ]
+        print(f"sample {len(images)} frames.")
+        return images
+
+    def __init__(self, max_size=MAX_IMAGE_SIZE):
         self.device = (
             "cuda"
             if torch.cuda.is_available()
@@ -27,6 +39,9 @@ class InternLMXComposer2(BaseTool):
             else "cpu"
         )
         self.max_size = max_size
+        # self._load_video = get_class_from_dynamic_module(
+        #     "ixc_utils.load_video", self._HF_MODEL
+        # )
         self._frame2img = get_class_from_dynamic_module(
             "ixc_utils.frame2img", self._HF_MODEL
         )
@@ -47,21 +62,19 @@ class InternLMXComposer2(BaseTool):
     def __call__(
         self,
         image: Image.Image | None,
-        video:  VideoNumpy | None,
+        video: VideoNumpy | None,
         prompt: str,
     ) -> str:
-        if  image is None and video is None:
+        if image is None and video is None:
             raise ValueError("Either 'image' or 'video' must be provided.")
         if image is not None and video is not None:
             raise ValueError("Only one of 'image' or 'video' can be provided.")
+
         if image is not None:
             image = self._transform_image(image, self.max_size)
         elif video is not None:
-            print("Video shape:", video.shape)
-            # video = [self._transform_image(image, self.max_size) for image in video]
-            video = [print("img_shape: ", image.shape) for image in video]
-            # 32 frames is a ~60s video clip
-            if len(video) > 32:
+            video = self._process_video(video)
+            if len(video) > self.MAX_NUMBER_OF_FRAMES:
                 raise ValueError("Video is too long")
             image = self._frame2img(video, self._get_font())
             image = self._video_transform(image)
