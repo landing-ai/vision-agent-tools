91f671a0a5ca5663cb7f48b41ed3f4fe84940081
Dillon Laird <dillonalaird@gmail.com>
2024-08-22 15:56:36 -0700

    Fix/florence2 sam2 prompt (#40)
    * fixed issue with florence2 multi-prompts
    
    * updated tests
    
    ---------
    
    Co-authored-by: Camilo Zapata <camilo@landing.ai>

--- vision_agent_tools/models/florence2_sam2.py
+++ vision_agent_tools/models/florence2_sam2.py
@@ -60,52 +60,52 @@ class Florence2SAM2(BaseMLModel):
 
     @torch.inference_mode()
     def get_bbox_and_mask(
-        self, image: Image.Image, prompts: list[str], return_mask: bool = True
+        self, prompt: str, image: Image.Image, return_mask: bool = True
     ) -> dict[int, ImageBboxAndMaskLabel]:
         objs = {}
         self.image_predictor.set_image(np.array(image, dtype=np.uint8))
         annotation_id = 0
-        for prompt in prompts:
-            with torch.autocast(device_type=self.device, dtype=torch.float16):
-                bboxes = self.florence2(
-                    image, PromptTask.CAPTION_TO_PHRASE_GROUNDING, prompt
-                )[PromptTask.CAPTION_TO_PHRASE_GROUNDING]["bboxes"]
-            if return_mask:
-                with torch.autocast(device_type=self.device, dtype=torch.bfloat16):
-                    masks, _, _ = self.image_predictor.predict(
-                        point_coords=None,
-                        point_labels=None,
-                        box=bboxes,
-                        multimask_output=False,
-                    )
-            for i in range(len(bboxes)):
-                objs[annotation_id] = ImageBboxAndMaskLabel(
-                    bounding_box=bboxes[i],
-                    mask=(
-                        masks[i, 0, :, :] if len(masks.shape) == 4 else masks[i, :, :]
-                    )
-                    if return_mask
-                    else None,
-                    label=prompt,
+        with torch.autocast(device_type=self.device, dtype=torch.float16):
+            preds = self.florence2(
+                image, PromptTask.CAPTION_TO_PHRASE_GROUNDING, prompt
+            )[PromptTask.CAPTION_TO_PHRASE_GROUNDING]
+        preds = [{"bbox": preds["bboxes"][i], "label": preds["labels"][i]} for i in range(len(preds["labels"]))]
+        if return_mask:
+            with torch.autocast(device_type=self.device, dtype=torch.bfloat16):
+                masks, _, _ = self.image_predictor.predict(
+                    point_coords=None,
+                    point_labels=None,
+                    box=[elt["bbox"] for elt in preds],
+                    multimask_output=False,
                 )
-                annotation_id += 1
+        for i in range(len(preds)):
+            objs[annotation_id] = ImageBboxAndMaskLabel(
+                bounding_box=preds[i]["bbox"],
+                mask=(
+                    masks[i, 0, :, :] if len(masks.shape) == 4 else masks[i, :, :]
+                )
+                if return_mask
+                else None,
+                label=preds[i]["label"],
+            )
+            annotation_id += 1
         return objs
 
     @torch.inference_mode()
     def handle_image(
-        self, image: Image.Image, prompts: list[str]
+        self, prompt: str, image: Image.Image,
     ) -> dict[int, dict[int, ImageBboxAndMaskLabel]]:
         self.image_predictor.reset_predictor()
-        objs = self.get_bbox_and_mask(image.convert("RGB"), prompts)
+        objs = self.get_bbox_and_mask(prompt, image.convert("RGB"))
         return {0: objs}
 
     @torch.inference_mode()
     def handle_video(
-        self, video: VideoNumpy, prompts: list[str]
+        self, prompt: str, video: VideoNumpy,
     ) -> dict[int, dict[int, MaskLabel]]:
         self.image_predictor.reset_predictor()
         objs = self.get_bbox_and_mask(
-            Image.fromarray(video[0]).convert("RGB"), prompts, return_mask=False
+            prompt, Image.fromarray(video[0]).convert("RGB"), return_mask=False
         )
         with torch.autocast(device_type=self.device, dtype=torch.bfloat16):
             inference_state = self.video_predictor.init_state(video=video)
@@ -135,7 +135,7 @@ class Florence2SAM2(BaseMLModel):
                     for i, out_obj_id in enumerate(out_obj_ids)
                 }
             self.video_predictor.reset_state(inference_state)
-            return video_segments
+        return video_segments
 
     @validate_call(config={"arbitrary_types_allowed": True})
     @torch.inference_mode()
@@ -157,14 +157,16 @@ class Florence2SAM2(BaseMLModel):
             1: ...
         }
         """
+
+        prompt = ", ".join(prompts)
         if image is None and video is None:
             raise ValueError("Either 'image' or 'video' must be provided.")
         if image is not None and video is not None:
             raise ValueError("Only one of 'image' or 'video' can be provided.")
 
         if image is not None:
-            return self.handle_image(image, prompts)
+            return self.handle_image(prompt, image)
         elif video is not None:
             assert video.ndim == 4, "Video should have 4 dimensions"
-            return self.handle_video(video, prompts)
+            return self.handle_video(prompt, video)
         # No need to raise an error here, the validatie_call decorator will take care of it
