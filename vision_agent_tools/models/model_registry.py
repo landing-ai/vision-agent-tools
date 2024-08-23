0e6f0b5657a69d40ebf49b83ca7e78ee6038a6ab
Hugo Honda <hugohondaf@gmail.com>
2024-08-23 16:16:22 -0300

    Fix: change the tools and registry logic for multiple model calling

--- vision_agent_tools/models/model_registry.py
+++ vision_agent_tools/models/model_registry.py
@@ -1,26 +1,18 @@
-from typing import List, Dict, Type, Callable
-from enum import Enum
+from typing import Dict, Callable
 from vision_agent_tools.shared_types import BaseMLModel
 from vision_agent_tools.models.florencev2 import Florencev2
 from vision_agent_tools.models.owlv2 import Owlv2
-
-
-class TextToObjectDetectionModel(str, Enum):
-    FLORENCEV2 = "florencev2"
-    OWLV2 = "owlv2"
+from vision_agent_tools.models.florencev2_qa import FlorenceQA
 
 
 MODEL_REGISTRY: Dict[str, Callable[[], BaseMLModel]] = {
     "florencev2": Florencev2,
     "owlv2": Owlv2,
-}
-
-TASK_MODEL_MAP: Dict[Enum, List[str]] = {
-    TextToObjectDetectionModel: [model.value for model in TextToObjectDetectionModel]
+    "florencev2_qa": FlorenceQA,
 }
 
 
-def get_model_class(model_name: str, task: Type[Enum]) -> BaseMLModel:
+def get_model_class(model_name: str) -> BaseMLModel:
     """
     Retrieve a model from the registry based on the model name and task
 
@@ -34,11 +26,6 @@ def get_model_class(model_name: str, task: Type[Enum]) -> BaseMLModel:
     Raises:
         ValueError: If the model is not registered or is not valid for the given task
     """
-
-    if model_name not in TASK_MODEL_MAP[task]:
-        raise ValueError(
-            f"Model '{model_name}' is not valid for the task '{task.__name__}'."
-        )
 
     model_class = MODEL_REGISTRY.get(model_name)
 
--- vision_agent_tools/tools/text_to_object_detection.py
+++ vision_agent_tools/tools/text_to_object_detection.py
@@ -1,13 +1,18 @@
 from typing import List, Any
+from enum import Enum
 from PIL import Image
 from pydantic import BaseModel
 from vision_agent_tools.shared_types import BaseTool
 from vision_agent_tools.models.model_registry import get_model_class
-from vision_agent_tools.models.model_registry import TextToObjectDetectionModel
 
 
 class TextToObjectDetectionOutput(BaseModel):
     output: Any
+
+
+class TextToObjectDetectionModel(str, Enum):
+    FLORENCEV2 = "florencev2"
+    OWLV2 = "owlv2"
 
 
 class TextToObjectDetection(BaseTool):
@@ -15,9 +20,12 @@ class TextToObjectDetection(BaseTool):
     Tool to perform object detection based on text prompts using a specified ML model
     """
 
-    def __init__(self, model: str):
-        self.model_name = model
-        model_class = get_model_class(model_name=model, task=TextToObjectDetectionModel)
+    def __init__(self, model: TextToObjectDetectionModel):
+        if model not in TextToObjectDetectionModel._value2member_map_:
+            raise ValueError(
+                f"Model '{model}' is not a valid model for {self.__class__.__name__}."
+            )
+        model_class = get_model_class(model_name=model)
         model_instance = model_class()
         super().__init__(model=model_instance)
 
