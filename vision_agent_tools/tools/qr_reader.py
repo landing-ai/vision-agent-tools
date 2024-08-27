e1eb768d552e10e64f30b96682f8f3bf73c30fbc
Hugo Honda <hugohondaf@gmail.com>
2024-08-27 16:57:14 -0300

    Fix: OCR to QR

--- vision_agent_tools/tools/qr_reader.py
+++ vision_agent_tools/tools/qr_reader.py
@@ -5,21 +5,21 @@ from vision_agent_tools.shared_types import BaseTool
 from vision_agent_tools.models.model_registry import get_model_class
 
 
-class OCROutput(BaseModel):
+class QRReaderOutput(BaseModel):
     text: str
 
 
-class OCRModel(str, Enum):
+class QRReaderModel(str, Enum):
     QR_READER = "qr_reader"
 
 
-class OCR(BaseTool):
+class QRReader(BaseTool):
     """
-    Tool to perform Optical Character Recognition (OCR) on an image.
+    Tool to perform QR reading on an image.
     """
 
-    def __init__(self, model: OCRModel):
-        if model not in OCRModel._value2member_map_:
+    def __init__(self, model: QRReaderModel):
+        if model not in QRReaderModel._value2member_map_:
             raise ValueError(
                 f"Model '{model}' is not a valid model for {self.__class__.__name__}."
             )
@@ -27,7 +27,7 @@ class OCR(BaseTool):
         model_instance = model_class()
         super().__init__(model=model_instance)
 
-    def __call__(self, image: Image.Image) -> OCROutput:
+    def __call__(self, image: Image.Image) -> QRReaderOutput:
         result = self.model(image=image)
 
         if isinstance(result, list) and len(result) > 0:
@@ -35,4 +35,4 @@ class OCR(BaseTool):
         else:
             detected_text = ""
 
-        return OCROutput(text=detected_text)
+        return QRReaderOutput(text=detected_text)
