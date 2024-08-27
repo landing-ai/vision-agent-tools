0bde6730c765b442479d6775cab0bd740eec32e7
Hugo Honda <hugohondaf@gmail.com>
2024-08-27 19:17:12 -0300

    Fix: update registry

--- vision_agent_tools/models/model_registry.py
+++ vision_agent_tools/models/model_registry.py
@@ -1,40 +1,26 @@
 from typing import Dict, Callable
 from vision_agent_tools.shared_types import BaseMLModel
-from vision_agent_tools.models.florencev2 import (
-    Florencev2,
-)  # Florencev2: interpret simple text prompts to perform tasks like captioning, object detection, and segmentation
-from vision_agent_tools.models.owlv2 import Owlv2  # Owlv2: object detection
-from vision_agent_tools.models.qr_reader import (
-    QRReader,
-)  # QRReader: QR code reading
-from vision_agent_tools.models.nshot_counting import (
-    NShotCounting,
-)  # NShotCounting: object counting using the zeroshot and n-shot versions of LOCA model
-from vision_agent_tools.models.nsfw_classification import (
-    NSFWClassification,
-)  # NSFWClassification: classification for NSFW (Not Safe for Work) images
+from vision_agent_tools.models.florencev2 import Florencev2
+from vision_agent_tools.models.owlv2 import Owlv2
+from vision_agent_tools.models.qr_reader import QRReader
+from vision_agent_tools.models.nshot_counting import NShotCounting
+from vision_agent_tools.models.nsfw_classification import NSFWClassification
 from vision_agent_tools.models.controlnet_aux import Image2Pose
-from vision_agent_tools.models.internlm_xcomposer2 import (
-    InternLMXComposer2,
-)  # InternLMXComposer2: text to image comprehension and composition
-from vision_agent_tools.models.clip_media_sim import (
-    CLIPMediaSim,
-)  # ClipMediaSim: video and target (image or text) to frames that are most similar to the target
-from vision_agent_tools.models.depth_anything_v2 import (
-    DepthAnythingV2,
-)  # DepthAnythingV2: depth estimation
+from vision_agent_tools.models.internlm_xcomposer2 import InternLMXComposer2
+from vision_agent_tools.models.clip_media_sim import CLIPMediaSim
+from vision_agent_tools.models.depth_anything_v2 import DepthAnythingV2
 
 
 MODEL_REGISTRY: Dict[str, Callable[[], BaseMLModel]] = {
-    "florencev2": Florencev2,
-    "owlv2": Owlv2,
-    "qr_reader": QRReader,
-    "nshot_counting": NShotCounting,
-    "nsfw_classification": NSFWClassification,
+    "florencev2": Florencev2,  # Florencev2: interpret simple text prompts to perform tasks like captioning, object detection, and segmentation
+    "owlv2": Owlv2,  # Owlv2: object detection
+    "qr_reader": QRReader,  # QRReader: QR code reading
+    "nshot_counting": NShotCounting,  # NShotCounting: object counting using the zeroshot and n-shot versions of LOCA model
+    "nsfw_classification": NSFWClassification,  # NSFWClassification: classification for NSFW (Not Safe for Work) images
     "image2pose": Image2Pose,
-    "internlm_xcomposer2": InternLMXComposer2,
-    "clip_media_sim": CLIPMediaSim,
-    "depth_anything_v2": DepthAnythingV2,
+    "internlm_xcomposer2": InternLMXComposer2,  # InternLMXComposer2: text to image comprehension and composition
+    "clip_media_sim": CLIPMediaSim,  # ClipMediaSim: video and target (image or text) to frames that are most similar to the target
+    "depth_anything_v2": DepthAnythingV2,  # DepthAnythingV2: depth estimation
 }
 
 
