27652f05ceeba9815ef62d21052f00aeca85409c
Hugo Honda <hugohondaf@gmail.com>
2024-08-28 21:55:04 -0300

    Fix: documentation and parameters

--- vision_agent_tools/models/model_registry.py
+++ vision_agent_tools/models/model_registry.py
@@ -35,24 +35,23 @@ MODEL_REGISTRY: Dict[str, Callable[[], BaseMLModel]] = {
 
 def get_model_class(model_name: str) -> BaseMLModel:
     """
-    Retrieve a model from the registry based on the model name and task
+    Retrieve a model from the registry based on the model name
 
     Args:
         model_name (str): The name of the model to retrieve
-        task (Type[Enum]): The enum representing the valid models for a specific task
 
     Returns:
         BaseMLModel: An instance of the requested model
 
     Raises:
-        ValueError: If the model is not registered or is not valid for the given task
+        ValueError: If the model is not registered or is not valid
     """
 
     model_class = MODEL_REGISTRY.get(model_name)
 
     if not model_class:
         raise ValueError(
-            f"Model '{model_name}' is not registered in the model registry."
+            f"Model '{model_name}' is not registered in the model registry"
         )
 
     return model_class
