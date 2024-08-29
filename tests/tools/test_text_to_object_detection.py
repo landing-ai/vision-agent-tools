932afe102f369910f7d190d50ac5eca4e6651ebb
hrnn <hernan.payrumani@landing.ai>
2024-08-28 20:35:52 -0400

    fixed unit test

--- tests/tools/test_text_to_object_detection.py
+++ tests/tools/test_text_to_object_detection.py
@@ -1,4 +1,5 @@
 from PIL import Image
+
 from vision_agent_tools.tools.text_to_object_detection import TextToObjectDetection
 
 
@@ -9,9 +10,9 @@ def test_successful_text_to_object_detection():
     image = Image.open(f"tests/tools/data/owlv2/{test_image}")
 
     tool = TextToObjectDetection(model="owlv2")
-    output = tool(image=image, prompts=prompts)
+    output = tool(image=image, prompts=prompts)[0].output
 
     assert len(output) > 0
 
-    for pred in output:
+    for pred in output[0]:
         assert pred.label == "a photo of a cat"
