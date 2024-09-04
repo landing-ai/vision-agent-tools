4093e983ccf5317a4d63edce3c06db281ed94bdb
junjie-landing <81201171+junjie-landing@users.noreply.github.com>
2024-09-04 10:17:06 -0700

    fix image in unitest x 2

--- vision_agent_tools/models/florencev2.py
+++ vision_agent_tools/models/florencev2.py
@@ -57,7 +57,7 @@ class Florencev2(BaseMLModel):
     """
 
     def _process_image(self, image: Image.Image) -> Image.Image:
-        return image.convert("RGB")
+        return image.convert(image)
 
     def _process_video(self, images: VideoNumpy) -> list[Image.Image]:
         return [self._process_image(Image.fromarray(arr)) for arr in images]
