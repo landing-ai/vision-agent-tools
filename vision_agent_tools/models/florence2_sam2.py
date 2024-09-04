15022df589530cb2f98946974f172111d9650508
CamiloInx <ca-mi-lo-iral@hotmail.com>
2024-09-04 15:43:22 +0000

    add chunk inference

--- vision_agent_tools/models/florence2_sam2.py
+++ vision_agent_tools/models/florence2_sam2.py
@@ -69,8 +69,10 @@ class Florence2SAM2(BaseMLModel):
             preds = self.florence2(
                 image=image, task=PromptTask.CAPTION_TO_PHRASE_GROUNDING, prompt=prompt,
             )[PromptTask.CAPTION_TO_PHRASE_GROUNDING]
-        preds = [{"bbox": preds["bboxes"][i], "label": preds["labels"][i]}
-                 for i in range(len(preds["labels"]))]
+        preds = [
+            {"bbox": preds["bboxes"][i], "label": preds["labels"][i]}
+            for i in range(len(preds["labels"]))
+        ]
         if return_mask:
             with torch.autocast(device_type=self.device, dtype=torch.bfloat16):
                 masks, _, _ = self.image_predictor.predict(
@@ -82,10 +84,7 @@ class Florence2SAM2(BaseMLModel):
         for i in range(len(preds)):
             objs[annotation_id] = ImageBboxAndMaskLabel(
                 bounding_box=preds[i]["bbox"],
-                mask=(
-                    masks[i, 0, :, :] if len(
-                        masks.shape) == 4 else masks[i, :, :]
-                )
+                mask=(masks[i, 0, :, :] if len(masks.shape) == 4 else masks[i, :, :])
                 if return_mask
                 else None,
                 label=preds[i]["label"],
@@ -95,7 +94,9 @@ class Florence2SAM2(BaseMLModel):
 
     @torch.inference_mode()
     def handle_image(
-        self, prompt: str, image: Image.Image,
+        self,
+        prompt: str,
+        image: Image.Image,
     ) -> dict[int, dict[int, ImageBboxAndMaskLabel]]:
         self.image_predictor.reset_predictor()
         objs = self.get_bbox_and_mask(prompt, image.convert("RGB"))
@@ -103,41 +104,57 @@ class Florence2SAM2(BaseMLModel):
 
     @torch.inference_mode()
     def handle_video(
-        self, prompt: str, video: VideoNumpy,
-    ) -> dict[int, dict[int, MaskLabel]]:
-        self.image_predictor.reset_predictor()
-        objs = self.get_bbox_and_mask(
-            prompt, Image.fromarray(video[0]).convert("RGB"), return_mask=False
-        )
+        self,
+        prompt: str,
+        video: VideoNumpy,
+        step: int = 20,
+    ) -> tuple[
+        dict[int, dict[int, MaskLabel]], dict[int, dict[int, ImageBboxAndMaskLabel]]
+    ]:
+        video_shape = video.shape
+        video_segments = {}
+        image_predictions = {}
+
         with torch.autocast(device_type=self.device, dtype=torch.bfloat16):
             inference_state = self.video_predictor.init_state(video=video)
-            for annotation_id in objs:
-                _, _, out_mask_logits = self.video_predictor.add_new_points_or_box(
-                    inference_state=inference_state,
-                    frame_idx=0,
-                    obj_id=annotation_id,
-                    box=objs[annotation_id].bounding_box,
+
+            for start_frame_idx in range(0, video_shape[0], step):
+                self.image_predictor.reset_predictor()
+                objs = self.get_bbox_and_mask(
+                    prompt,
+                    Image.fromarray(video[start_frame_idx]).convert("RGB"),
+                    return_mask=False,
                 )
-
-            annotation_id_to_label = {}
-            for annotation_id in objs:
-                annotation_id_to_label[annotation_id] = objs[annotation_id].label
-
-            video_segments = {}
-            for (
-                out_frame_idx,
-                out_obj_ids,
-                out_mask_logits,
-            ) in self.video_predictor.propagate_in_video(inference_state):
-                video_segments[out_frame_idx] = {
-                    out_obj_id: MaskLabel(
-                        mask=(out_mask_logits[i][0] > 0.0).cpu().numpy(),
-                        label=annotation_id_to_label[out_obj_id],
+                image_predictions[start_frame_idx] = objs
+                # prompt grounding dino to get the box coordinates on specific frame
+                # print("start_frame_idx", start_frame_idx)
+                self.video_predictor.reset_state(inference_state)
+                annotation_id_to_label = {}
+                for annotation_id in objs:
+                    annotation_id_to_label[annotation_id] = objs[annotation_id].label
+                    _, _, out_mask_logits = self.video_predictor.add_new_points_or_box(
+                        inference_state=inference_state,
+                        frame_idx=start_frame_idx,
+                        obj_id=annotation_id,
+                        box=objs[annotation_id].bounding_box,
                     )
-                    for i, out_obj_id in enumerate(out_obj_ids)
-                }
-            self.video_predictor.reset_state(inference_state)
-        return video_segments
+
+                for (
+                    out_frame_idx,
+                    out_obj_ids,
+                    out_mask_logits,
+                ) in self.video_predictor.propagate_in_video(
+                    inference_state, start_frame_idx, step
+                ):
+                    video_segments[out_frame_idx] = {
+                        out_obj_id: MaskLabel(
+                            mask=(out_mask_logits[i][0] > 0.0).cpu().numpy(),
+                            label=annotation_id_to_label[out_obj_id],
+                        )
+                        for i, out_obj_id in enumerate(out_obj_ids)
+                    }
+                self.video_predictor.reset_state(inference_state)
+        return (video_segments, image_predictions)
 
     @validate_call(config={"arbitrary_types_allowed": True})
     @torch.inference_mode()
@@ -146,6 +163,7 @@ class Florence2SAM2(BaseMLModel):
         prompts: list[str],
         image: Image.Image | None = None,
         video: VideoNumpy | None = None,
+        step: int | None = 20,
     ) -> dict[int, dict[int, ImageBboxAndMaskLabel | MaskLabel]]:
         """Returns a dictionary where the first key is the frame index then an annotation
         ID, then an object with the mask, label and possibly bbox (for images) for each
@@ -170,5 +188,5 @@ class Florence2SAM2(BaseMLModel):
             return self.handle_image(prompt, image)
         elif video is not None:
             assert video.ndim == 4, "Video should have 4 dimensions"
-            return self.handle_video(prompt, video)
+            return self.handle_video(prompt, video, step)
         # No need to raise an error here, the validatie_call decorator will take care of it
