8824e94bb75803a4db3ba04c69871dc9d1d98000
hrnn <hernan.payrumani@landing.ai>
2024-09-16 00:00:52 -0400

    feat: grayscale output support for depth anytthing

--- tests/models/test_depth_anything_v2.py
+++ tests/models/test_depth_anything_v2.py
@@ -1,3 +1,4 @@
+import numpy as np
 from PIL import Image
 
 from vision_agent_tools.models.depth_anything_v2 import DepthAnythingV2
@@ -10,3 +11,19 @@ def test_successful_depth_estimation():
     depth = DepthAnythingV2()
     results = depth(image)
     assert len(results.map) > 0
+
+
+def test_depth_grayscale_true():
+    test_image = "tomatoes.jpg"
+    image = Image.open(f"tests/tools/data/loca/{test_image}")
+
+    depth = DepthAnythingV2()
+
+    results = depth(image=image, grayscale=True)
+
+    # Check that results.map is a 3D numpy array
+    assert isinstance(results.map, np.ndarray)
+    assert results.map.ndim == 2, f"Expected 2D array, got {results.map.ndim}D array"
+
+    # Check that image values are in the range [0, 255]
+    assert np.all((results.map >= 0) & (results.map <= 255))
