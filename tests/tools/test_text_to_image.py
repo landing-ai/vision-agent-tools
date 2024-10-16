446119dfa2666b7799e15d5f9f5f6abebde36ec4
Hugo Honda <hugohondaf@gmail.com>
2024-10-16 15:54:44 -0300

    update tests

--- tests/tools/test_text_to_image.py
+++ tests/tools/test_text_to_image.py
@@ -1,28 +1,8 @@
-import pytest
 from PIL import Image
 
 from vision_agent_tools.models.flux1 import Flux1Task
 
 
-@pytest.mark.skip(
-    reason="FIX THIS TEST. This test is incorrect, the configs are not being used."
-)
-def test_successful_image_generation(tool):
-    prompt = "cat wizard, Pixar style"
-
-    result = tool(
-        task=Flux1Task.IMAGE_GENERATION,
-        prompt=prompt,
-    )
-
-    assert result is not None
-    assert result.mode == "RGB"
-    assert result.size == (32, 32)
-
-
-@pytest.mark.skip(
-    reason="FIX THIS TEST. This test is incorrect, the configs are not being used."
-)
 def test_successful_image_mask_inpainting(tool):
     prompt = "cat wizard, Pixar style"
     image = Image.open("tests/shared_data/images/chihuahua.png")
@@ -33,6 +13,12 @@ def test_successful_image_mask_inpainting(tool):
         prompt=prompt,
         image=image,
         mask_image=mask_image,
+        height=32,
+        width=32,
+        num_inference_steps=1,
+        guidance_scale=7,
+        strength=0.85,
+        seed=42,
     )
 
     assert result is not None
@@ -40,13 +26,19 @@ def test_successful_image_mask_inpainting(tool):
     assert result.size == (32, 32)
 
 
-# @pytest.fixture(scope="session")
-# def tool():
-#     flux_config = Flux1Config(
-#         height=32,
-#         width=32,
-#         num_inference_steps=1,
-#         max_sequence_length=256,
-#     )
+def test_successful_image_generation(tool):
+    prompt = "cat wizard, Pixar style"
 
-#     return TextToImage(model=TextToImageModel.FLUX1, model_config=flux_config)
+    result = tool(
+        task=Flux1Task.IMAGE_GENERATION,
+        prompt=prompt,
+        height=32,
+        width=32,
+        guidance_scale=0.5,
+        num_inference_steps=1,
+        seed=42,
+    )
+
+    assert result is not None
+    assert result.mode == "RGB"
+    assert result.size == (32, 32)
