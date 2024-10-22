6f30f53b347f53ae07faafdf6790783678cdd83a
Dayanne Fernandes <dayannefernandesc@gmail.com>
2024-10-22 18:00:40 +0000

    text to od tests

--- tests/tools/test_text_to_object_detection.py
+++ tests/tools/test_text_to_object_detection.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 from PIL import Image
 
 from vision_agent_tools.tools.text_to_object_detection import (
@@ -15,7 +16,7 @@ def test_text_to_object_detection_owlv2(shared_tool_owlv2):
 
     response = shared_tool_owlv2(prompts, images=[image])
 
-    assert response == [
+    expected_response = [
         {
             "scores": [
                 0.6933691501617432,
@@ -32,6 +33,7 @@ def test_text_to_object_detection_owlv2(shared_tool_owlv2):
             ],
         }
     ]
+    check_results(response, expected_response)
 
 
 def test_text_to_object_detection_florence2(shared_tool_florence2):
@@ -71,3 +73,43 @@ def shared_tool_owlv2():
 @pytest.fixture(scope="module")
 def shared_tool_florence2():
     return TextToObjectDetection(model=TextToObjectDetectionModel.FLORENCE2)
+
+
+def check_results(
+    response, expected_response, amount_of_matches: int = None, flex: int = 1
+):
+    # sort the results by score to make the comparison easier
+    response = sorted(response, key=lambda x: x["scores"], reverse=True)
+    expected_response = sorted(
+        expected_response, key=lambda x: x["scores"], reverse=True
+    )
+
+    for item, expected_result in zip(response, expected_response):
+        if amount_of_matches is None:
+            amount_of_matches = len(expected_result["bboxes"]) - flex
+
+        assert (
+            abs(len(item["bboxes"]) - len(expected_result["bboxes"]))
+            <= amount_of_matches
+        )
+        for bbox, expected_bbox in zip(item["bboxes"], expected_result["bboxes"]):
+            assert np.allclose(bbox, expected_bbox, rtol=1, atol=1)
+
+        assert (
+            abs(len(item["labels"]) - len(expected_result["labels"]))
+            <= amount_of_matches
+        )
+        count_equal_labels = 0
+        for lab, expected_lab in zip(item["labels"], expected_result["labels"]):
+            if lab == expected_lab:
+                count_equal_labels += 1
+        assert (
+            abs(count_equal_labels - len(item["labels"])) <= amount_of_matches
+        ), f"{item['labels']}, {expected_result['labels']}"
+
+        assert (
+            abs(len(item["scores"]) - len(expected_result["scores"]))
+            <= amount_of_matches
+        )
+        for bbox, expected_bbox in zip(item["scores"], expected_result["scores"]):
+            np.testing.assert_almost_equal(bbox, expected_bbox, decimal=1)
