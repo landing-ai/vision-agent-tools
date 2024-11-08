ace6f5049216cf47f053cf11bb39192af9c629c6
hrnn <hernan.payrumani@landing.ai>
2024-11-08 22:21:59 +0000

    fixed error

--- vision_agent_tools/helpers/filters.py
+++ vision_agent_tools/helpers/filters.py
@@ -214,7 +214,7 @@ def _filter_invalid_bboxes(
 
     invalid_bboxes = []
 
-    for bbox in zip(predictions[bboxes_key]):
+    for bbox in predictions[bboxes_key]:
         x1, y1, x2, y2 = bbox
         if not (0 <= x1 < x2 <= width and 0 <= y1 < y2 <= height):
             invalid_bboxes.append(bbox)
